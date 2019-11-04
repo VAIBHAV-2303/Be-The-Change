09095314620eeb107dd764aab9c2a0a69ef6950b
akshayg08 <asgoindani@gmail.com>
2019-11-04 21:37:49 +0530

    Restructured everything

---
+++ src/maze.js
@@ -0,0 +1,55 @@
+import {Scene} from "phaser"
+
+class pastWorld extends Scene{
+    preload ()
+    {
+        this.load.image('floor', 'assets/floor.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('groundv', 'assets/platformv.png');
+    }
+    
+    create ()
+    {
+        this.add.image(920, 480, 'floor');
+        this.platforms = this.physics.add.staticGroup();
+        
+        // Creatin the maze
+        this.platforms.create(530, 460, 'groundv').setScale(0.5, 1.7).refreshBody();
+        this.platforms.create(802, 120, 'ground').setScale(1.4, 0.5).refreshBody();
+        this.platforms.create(1280, 120, 'ground').setScale(0.2, 0.5).refreshBody();
+        this.platforms.create(1312, 468.1, 'groundv').setScale(0.5, 1.7).refreshBody();
+        this.platforms.create(562, 805, 'ground').setScale(0.2, 0.5).refreshBody();
+        this.platforms.create(1020, 805, 'ground').setScale(1.5, 0.5).refreshBody();
+        this.platforms.create(572, 405, 'ground').setScale(0.25, 0.5).refreshBody();
+        this.platforms.create(620, 477, 'groundv').setScale(0.5, 0.4).refreshBody();
+        this.platforms.create(620, 210, 'groundv').setScale(0.5, 0.45).refreshBody();
+        this.platforms.create(620, 680, 'groundv').setScale(0.5, 0.2).refreshBody();
+        this.platforms.create(672, 292, 'ground').setScale(0.25, 0.5).refreshBody();
+        this.platforms.create(672, 549, 'ground').setScale(0.25, 0.5).refreshBody();
+        this.platforms.create(720, 581, 'groundv').setScale(0.5, 0.2).refreshBody();
+        this.platforms.create(720, 160, 'groundv').setScale(0.5, 0.2).refreshBody();
+        this.platforms.create(720, 772, 'groundv').setScale(0.55, 0.2).refreshBody();
+        this.platforms.create(720, 680, 'ground').setScale(0.5, 0.5).refreshBody();
+        this.platforms.create(800, 740, 'ground').setScale(0.4, 0.5).refreshBody();
+        this.platforms.create(810, 440, 'ground').setScale(0.4, 0.5).refreshBody();
+        this.platforms.create(810, 300, 'groundv').setScale(0.55, 0.4).refreshBody();
+        this.platforms.create(920, 300, 'ground').setScale(0.55, 0.4).refreshBody();
+        this.platforms.create(920, 300, 'ground').setScale(0.55, 0.4).refreshBody();
+        this.platforms.create(920, 227, 'ground').setScale(0.55, 0.4).refreshBody();
+        this.platforms.create(1000, 168, 'groundv').setScale(0.55, 0.28).refreshBody();
+        this.platforms.create(1160, 260, 'groundv').setScale(0.55, 0.7).refreshBody();
+        this.platforms.create(1160, 260, 'groundv').setScale(0.55, 0.7).refreshBody();
+        this.platforms.create(810, 520, 'groundv').setScale(0.55, 0.4).refreshBody();
+        this.platforms.create(1040, 680, 'ground').setScale(0.55, 0.5).refreshBody();
+        this.platforms.create(920, 592, 'ground').setScale(0.55, 0.5).refreshBody();
+        this.platforms.create(1109, 392, 'ground').setScale(0.3, 0.5).refreshBody();
+        this.platforms.create(1058, 460, 'groundv').setScale(0.55, 0.3).refreshBody();
+        this.platforms.create(1007, 512, 'ground').setScale(0.3, 0.5).refreshBody();
+        this.platforms.create(956, 450, 'groundv').setScale(0.55, 0.35).refreshBody();
+        this.platforms.create(885, 418, 'groundv').setScale(0.55, 0.15).refreshBody();
+        this.platforms.create(920, 385, 'ground').setScale(0.22, 0.5).refreshBody();
+        this.platforms.create(1180, 555, 'ground').setScale(0.24, 0.5).refreshBody();
+        this.platforms.create(1180, 555, 'groundv').setScale(0.55, 0.4).refreshBody();
+    }
+}
+export default maze
---
+++ src/pastWorld.js
@@ -0,0 +1,171 @@
+import {Scene} from "phaser"
+
+class pastWorld extends Scene{
+	
+	preload ()
+	{
+		this.load.image('bad_ground', 'assets/bad_ground.jpg');
+		this.load.image('museum', 'assets/museum.png');
+		this.load.image("bad_building", "assets/bad_building.png");
+		this.load.image("bad_building2", "assets/bad_building2.png");
+		this.load.image("bad_home", "assets/bad_home.png");
+		this.load.image("bad_park", "assets/bad_park.png");
+		this.load.image("bad_pool", "assets/bad_pool.png");
+		this.load.image("bad_tree", "assets/bad_tree.png");
+		this.load.spritesheet('dude', 'assets/spr_fembase_walk_strip16.png', { frameWidth: 32, frameHeight: 64});
+	}
+
+
+	create ()
+	{
+		// Placing objects of the game
+		this.createGround();
+		this.createMuseum();
+		this.createObjects();
+		this.createPlayer();
+		
+		// Adjusting camera so that it follows the player
+		this.cameras.main.x = -500;
+		this.cameras.main.y = -500;
+		this.cameras.main.startFollow(this.player);
+		this.cursors = this.input.keyboard.createCursorKeys();
+
+		// Adding colliders
+		this.physics.add.collider(this.player, this.bad_building);
+		this.physics.add.collider(this.player, this.bad_building2);
+		this.physics.add.collider(this.player, this.bad_tree);
+		this.physics.add.collider(this.player, this.bad_home);
+		this.physics.add.collider(this.player, this.bad_park);
+		this.physics.add.collider(this.player, this.bad_pool);
+		
+		// Initial narrative text
+		this.storyText = this.add.text(100, 500, 'Lila fears the world will no longer be green.\
+		 The war has changed everything.\n A gloominess is wrapped around the atmosphere.\n \
+		 Everyone is in their homes and there is a \n general lack of freedom. \
+		 Lila \nis not happy with the current situation and she \n wishes to change it and \
+		 hence she goes out exploring the world.\n', { fontSize: '32px', fill: '#0D3107' });
+		setTimeout(() => {this.storyText.setText('');}, 10000);
+	}
+
+	createGround(){
+		this.bad_ground = this.physics.add.staticGroup();
+		this.bad_ground.create(1000, 1000, 'bad_ground');
+	}
+
+	createMuseum(){
+		this.museum = this.physics.add.staticGroup();
+		this.museum.create(1600, 1200, 'museum').setScale(0.8, 0.8).refreshBody();
+	}
+
+	createObjects(){
+		Y = [400, 1200, 800, 2000, 1600, 0];
+		this.bad_building = this.physics.add.staticGroup();
+		for(var i=0;i<6;i++){
+			this.bad_building.create(i*400, Y[i], 'bad_building');
+		}
+
+		Y = [1200, 400, 2000, 800, 0, 1600];
+		this.bad_building2 = this.physics.add.staticGroup();
+		for(var i=0;i<6;i++){
+			this.bad_building2.create(i*400, Y[i], 'bad_building2');
+		}
+
+		Y = [1600, 2000, 1200, 0, 800, 400];
+		this.bad_tree = this.physics.add.staticGroup();
+		for(var i=0;i<6;i++){
+			this.bad_tree.create(i*400, Y[i], 'bad_tree');
+		}
+
+		Y = [800, 0, 1600, 400, 2000];
+		this.bad_home = this.physics.add.staticGroup();
+		for(var i=0;i<5;i++){
+			this.bad_home.create(i*400, Y[i], 'bad_home');
+		}
+
+		Y = [0, 800, 400, 1600, 2000, 1200];
+		this.bad_park = this.physics.add.staticGroup();
+		for(var i=0;i<6;i++){
+			this.bad_park.create(i*400, Y[i], 'bad_park');
+		}
+
+		Y = [2000, 1600, 0, 1200, 400, 800];
+		this.bad_pool = this.physics.add.staticGroup();
+		for(var i=0;i<6;i++){
+			this.bad_pool.create(i*400, Y[i], 'bad_pool');
+		}
+	}
+
+	createPlayer(){
+		this.player = this.physics.add.sprite(400, 600, 'dude');
+		this.player.setBounce(0.2);
+		this.player.setCollideWorldBounds(true);
+
+		// Player animations
+		this.anims.create({
+			key: 'left',
+			frames: this.anims.generateFrameNumbers('dude', { start: 8, end: 11 }),
+			frameRate: 10,
+			repeat: -1
+		});
+		this.anims.create({
+			key: 'turn',
+			frames: [ { key: 'dude', frame: 4 } ],
+			frameRate: 20
+		});
+		this.anims.create({
+			key: 'right',
+			frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+			frameRate: 10,
+			repeat: -1
+		});
+		this.anims.create({
+			key: 'up',
+			frames: this.anims.generateFrameNumbers('dude', { start: 4, end: 7 }),
+			frameRate: 10,
+			repeat: -1
+		});
+		this.anims.create({
+			key: 'down',
+			frames: this.anims.generateFrameNumbers('dude', { start: 4, end: 7 }),
+			frameRate: 10,
+			repeat: -1
+		});
+	}
+
+	update()
+	{
+		// storyText.setText('');
+		// Defining player movement functions
+		if (this.cursors.right.isDown)
+		{	
+			this.player.setVelocityX(speed);
+			this.player.setVelocityY(0);
+			this.player.anims.play('right', true);
+		}
+		else if(this.cursors.left.isDown)
+		{
+			this.player.setVelocityX(-speed);
+			this.player.setVelocityY(0);
+			this.player.anims.play('left', true);
+		}
+		else if(this.cursors.up.isDown)
+		{
+			this.player.setVelocityY(-speed);
+			this.player.setVelocityX(0);
+			this.player.anims.play('up', true);
+		}
+		else if(this.cursors.down.isDown)
+		{
+			this.player.setVelocityY(speed);
+			this.player.setVelocityX(0);
+			this.player.anims.play('down', true);	
+		}
+		else
+		{
+			this.player.setVelocityX(0);
+			this.player.setVelocityY(0);
+			this.player.anims.play('turn');
+		}
+	}
+}
+export default pastWorld
